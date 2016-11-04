c7a84289b57de4769aecbafdba3231a50e72c0a5
Margie Roswell <mroswell@gmail.com>
2016-11-04 06:44:45 -0400

    bootstrap and social

---
+++ js/index3.js
@@ -0,0 +1,10 @@
+/**
+ * Created by marjorieroswell on 11/3/16.
+ */
+$(function(){
+  $('#video').css({ width: $(window).innerWidth() + 'px', height: $(window).innerHeight() + 'px' });
+
+  $(window).resize(function(){
+    $('#video').css({ width: $(window).innerWidth() + 'px', height: $(window).innerHeight() + 'px' });
+  });
+});
